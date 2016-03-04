314c73f09e8cc3b841983585657ac3b5b0b0bf69
Andrei Mihnevitš <andrei.mihn@gmail.com>
2016-03-04 14:58:01 +0200

    Define an interface for the operator's view renderer

---
+++ operator_presence/IOperatorViewRenderer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace operator_view
+{
+	/// \brief An interface for rendering the operator's entire view
+	///
+	/// This interface is an abstraction layer for the client - IOperatorView.
+	///
+	/// More than that, this interface can play a role of a strategy, which
+	/// defines the way of rendering the operator's view, e.g. there can be
+	/// different strategies to render the view into the operator's HMDs, or
+	/// even a strategy to render the view without considering any HMDs,
+	/// i.e. rendering simply to the screen.
+	///
+	/// On the other hand, this interface can be viewed as an adapter between
+	/// the IOperatorView and IOperatorRenderer interfaces.
+	///
+	class IOperatorViewRenderer
+	{
+	public:
+		virtual ~IOperatorViewRenderer() { }
+
+	public:
+		virtual void render() = 0;
+	};
+}

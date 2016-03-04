056b8311d9100fb20481463df715842d8c08e38b
Andrei Mihnevitš <andrei.mihn@gmail.com>
2016-03-04 14:02:59 +0200

    Rename IOperatorViewRendererObserver to IOperatorRendererObserver

--- operator_presence/IOperatorRenderer.h
+++ operator_presence/IOperatorRenderer.h
@@ -2,35 +2,20 @@
 
 namespace operator_view
 {
-	/// \brief An interface for rendering the operator's view
+	/// \brief An interface for rendering the operator
 	///
-	/// The operator has two eyes, so this interface allows to render the view to both eyes.
-	/// This means that the view will be divided in half when being displayed,
-	/// and each eye will get half of this view
-	/// The interface also allows for delayed initialization.
+	/// This interface is intended to be inherited by more concrete operator renderers.
+	/// Since the operator has two eyes, this interface allows rendering to each eye separately.
+	/// The interface also allows delayed initialization.
 	///
-	class IOperatorViewRenderer
+	class IOperatorRenderer
 	{
 	public:
-		virtual ~IOperatorViewRenderer() { }
+		virtual ~IOperatorRenderer() { }
 
 	public:
-		/// \brief An initialization of the view
-		///
-		/// This method allows to perform delayed initialization of the view.
-		/// The method accepts two parameters:
-		/// 1. the width of the view
-		/// 2. the height of the view
-		/// Consequently, in order to accomodate the view for both eyes,
-		/// the eye images should have the width of the view divided in half.
-		///
-		virtual void initialize(std::uint16_t width, std::uint16_t height) = 0;
+		virtual void initialize(std::uint16_t eyeResolutionWidth, std::uint16_t eyeResolutionHeight) = 0;
 
-		/// \brief Methods to render each eye separately
-		///
-		/// The methods should render their portion of the view,
-		/// the portion that was allocated for each of the eyes.
-		///
 		virtual void renderLeftEye() = 0;
 		virtual void renderRightEye() = 0;
 	};

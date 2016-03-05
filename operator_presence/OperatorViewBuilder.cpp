0ed47b21b013241eaa4e33055ab3c70f0076f0a4
Andrei Mihnevitš <andrei.mihn@gmail.com>
2016-03-05 22:35:48 +0200

    Implement the operator view builder

--- operator_presence/OperatorViewBuilder.cpp
+++ operator_presence/OperatorViewBuilder.cpp
@@ -2,53 +2,51 @@
 
 #include "OperatorViewBuilder.h"
 
-#include "IOperatorModel.h"
-
-#include "IOperatorRendererFactory.h"
-
 #include "IOperatorDisplayRenderer.h"
-#include "IOperatorVisionRenderer.h"
-
-#include "IOperatorViewRendererFactory.h"
-
-#include "IOperatorOculusRiftRenderer.h"
-
-#include "OperatorView.h"
+#include "IOperatorModel.h"
+#include "IOperatorViewFactory.h"
+#include "IOperatorVisionDecorator.h"
 
 namespace operator_view
 {
-	OperatorViewBuilder::OperatorViewBuilder(std::shared_ptr<operator_model::IOperatorModel> operatorModel, std::unique_ptr<IOperatorViewRendererFactory> operatorViewRendererFactory, std::unique_ptr<IOperatorRendererFactory> operatorRendererFactory)
+	OperatorViewBuilder::OperatorViewBuilder(std::shared_ptr<operator_model::IOperatorModel> operatorModel, std::unique_ptr<IOperatorViewFactory> operatorViewFactory)
 		: m_operatorModel(operatorModel)
-		, m_operatorViewRendererFactory(std::move(operatorViewRendererFactory))
-		, m_operatorRendererFactory(std::move(operatorRendererFactory))
-		, m_operatorViewBuilt(false)
+		, m_operatorViewFactory(std::move(operatorViewFactory))
 	{
-		m_operatorView = std::shared_ptr<OperatorView>(new OperatorView);
+		//
+		// Compose what is a must for the view.
+		//
 
-		auto operatorDisplayRenderer = m_operatorRendererFactory->createOperatorDisplayRenderer(m_operatorView);
-		auto operatorVisionRenderer = m_operatorRendererFactory->createOperatorVisionRenderer(operatorDisplayRenderer, m_operatorView);
+		auto operatorDisplayRenderer = m_operatorViewFactory->createOperatorDisplayRenderer();
+		auto operatorVisionDecorator = m_operatorViewFactory->createOperatorVisionDecorator(operatorDisplayRenderer);
 		
-		m_operatorModel->registerObserver(operatorVisionRenderer);
-		m_operatorRenderer = operatorVisionRenderer;
+		m_operatorModel->registerObserver(operatorVisionDecorator);
+
+		m_renderer = operatorVisionDecorator;
 	}
 
-	void OperatorViewBuilder::displayHUD()
+	std::shared_ptr<IOperatorView> OperatorViewBuilder::build(Strategy strategy)
 	{
-		/* not yet implemented */
-	}
-
-	std::shared_ptr<IOperatorView> OperatorViewBuilder::build()
-	{
-		if (m_operatorViewBuilt)
+		// Take what has been built so far and apply the requested strategy.
+		//
+		switch (strategy)
 		{
-			assert(false); return m_operatorView;
+			case Strategy::OculusRift:
+			{
+				auto operatorViewStrategy = m_cachedOperatorOculusRiftStrategy.lock();
+
+				if (operatorViewStrategy == nullptr)
+				{
+					operatorViewStrategy = m_operatorViewFactory->createOperatorOculusRiftStrategy(m_renderer);
+					m_cachedOperatorOculusRiftStrategy = operatorViewStrategy;
+				}
+			}
+			break;
+
+			default:
+			{
+				assert(false);
+			}
 		}
-
-		auto operatorOculusRiftRenderer = m_operatorViewRendererFactory->createOperatorOculusRiftRenderer(m_operatorRenderer, m_operatorView);
-		m_operatorView->setRenderingStrategy(operatorOculusRiftRenderer);
-
-		m_operatorViewBuilt = true;
-
-		return m_operatorView;
 	}
 }
--- operator_presence/OperatorViewBuilder.h
+++ operator_presence/OperatorViewBuilder.h
@@ -7,46 +7,41 @@ namespace operator_model
 
 namespace operator_view
 {
-	class IOperatorRenderer;
-	class IOperatorRendererFactory;
 	class IOperatorView;
+	class IOperatorViewFactory;
 	class IOperatorViewRenderer;
-	class IOperatorViewRendererFactory;
-	class OperatorView;
 }
 
 namespace operator_view
 {
 	/// \brief An operator view builder
 	///
-	/// The class follows the Builder pattern and
-	/// is meant to hide the details of the rendering classes' construction.
-	///
-	/// The class must be instantiated through derived classes,
-	/// hence the protected constructor.
-	/// The only purpose of the derived classes is to supply a proper abstract factory objects
-	/// to this class, so that appropriate rendering objects - OpenGL, Direct3D, etc - can be created.
+	/// The class follows the Builder pattern
+	/// and is meant to hide the details of
+	/// the rendering classes' construction.
+	/// The class must be instantiated through
+	/// derived classes, hence the protected
+	/// constructor. The only purpose of the
+	/// derived classes is to supply a proper
+	/// abstract factory objects to this class,
+	/// so that appropriate rendering objects -
+	/// OpenGL, Direct3D, etc - can be created.
 	///
 	class OperatorViewBuilder
 	{
 	protected:
-		OperatorViewBuilder
-			(
-				std::shared_ptr<operator_model::IOperatorModel> operatorModel,
-				std::unique_ptr<IOperatorViewRendererFactory> operatorViewRendererFactory,
-				std::unique_ptr<IOperatorRendererFactory> operatorRendererFactory
-			);
+		OperatorViewBuilder(std::shared_ptr<operator_model::IOperatorModel> operatorModel, std::unique_ptr<IOperatorViewFactory> operatorViewFactory);
 
 	private:
 		std::shared_ptr<operator_model::IOperatorModel> m_operatorModel;
-
-		std::unique_ptr<IOperatorViewRendererFactory> m_operatorViewRendererFactory;
-		std::unique_ptr<IOperatorRendererFactory> m_operatorRendererFactory;
+		std::unique_ptr<IOperatorViewFactory> m_operatorViewFactory;
 
 	public:
-		/// \brief Additional views to display
+		/// \brief Additional views
 		///
-		void displayHUD();
+
+	public:
+		enum class Strategy { OculusRift };
 
 	public:
 		/// \brief The final result output
@@ -54,12 +49,11 @@ namespace operator_view
 		/// The method returns a pointer to the interface IOperatorView which is an
 		/// abstraction meant to be used by other system parts like the operator controller.
 		///
-		std::shared_ptr<IOperatorView> build();
+		std::shared_ptr<IOperatorView> build(Strategy strategy);
 
 	private:
-		std::shared_ptr<IOperatorRenderer> m_operatorRenderer;
-		std::shared_ptr<OperatorView> m_operatorView;
+		std::shared_ptr<IOperatorViewRenderer> m_renderer;
 
-		bool m_operatorViewBuilt;
+		std::weak_ptr<IOperatorOculusRiftStrategy> m_cachedOperatorOculusRiftStrategy;
 	};
 }

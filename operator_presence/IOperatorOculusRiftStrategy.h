8c7a4ff6ace5fe8bc5a4baead327517f0ef552e7
Andrei Mihnevitš <andrei.mihn@gmail.com>
2016-03-05 19:28:33 +0200

    Rename OperatorOculusRiftRenderer to OperatorOculusRiftStrategy

--- operator_presence/IOperatorOculusRiftStrategy.h
+++ operator_presence/IOperatorOculusRiftStrategy.h
@@ -9,12 +9,12 @@ namespace operator_view
 
 namespace operator_view
 {
-	/// \brief An interface for rendering the operator's view using Oculus Rift
+	/// \brief An interface for rendering the operator's view using Oculus Rift strategy
 	///
 	/// The reason to provide this layer of abstraction is that Oculus Rift
 	/// can be configured to use either OpenGL or Direct3D for rendering.
 	///
-	class IOperatorOculusRiftRenderer : public IOperatorViewStrategy
+	class IOperatorOculusRiftStrategy : public IOperatorViewStrategy
 	{
 	public:
 		virtual void registerObserver(std::shared_ptr<IOperatorViewObserver> observer) = 0;

8d7d00455751fcb1b383d8ee46800daad25806e6
Andrei Mihnevitš <andrei.mihn@gmail.com>
2016-03-05 17:45:43 +0200

    Start refactoring the operator view

--- operator_presence/IOperatorView.h
+++ operator_presence/IOperatorView.h
@@ -3,7 +3,6 @@
 namespace operator_view
 {
 	class IOperatorViewObserver;
-	class IOperatorViewRenderer;
 }
 
 namespace operator_view
@@ -13,21 +12,11 @@ namespace operator_view
 	public:
 		virtual ~IOperatorView() { }
 
-	private:
-		virtual void setRenderingStrategy(std::shared_ptr<IOperatorViewRenderer> renderingStrategy) = 0;
-
 	public:
-		virtual void open() = 0;
-		virtual void close() = 0;
-
-	public:
-		virtual void exec() = 0;
+		virtual void initialize() = 0;
+		virtual void render() = 0;
 
 	public:
 		virtual void registerObserver(std::shared_ptr<IOperatorViewObserver> observer) = 0;
-
-	private:
-		virtual void notifyHeadsetOrientationChanged(double yaw, double pitch, double roll) = 0;
-		virtual void notifyVisionSelected(std::uint32_t visionId) = 0;
 	};
 }
--- operator_presence/IOperatorViewObserver.h
+++ operator_presence/IOperatorViewObserver.h
@@ -9,6 +9,9 @@ namespace operator_view
 
 	public:
 		virtual void updateHeadsetOrientationChanged(double yaw, double pitch, double roll) = 0;
+		virtual void updateKeyPressed(Qt::Key key) = 0;
 		virtual void updateVisionSelected(std::uint32_t visionId) = 0;
+		virtual void updateWindowExposed(WId windowId) = 0;
+		virtual void updateWindowSizeChanged(std::uint16_t width, std::uint16_t height) = 0;
 	};
 }
